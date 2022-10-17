5cf02b826360b75d445839a9a081745706a8833e
Samuel Son <Samuel.Daehan@gmail.com>
2022-10-17 10:17:20 -0500

    update

---
+++ week2/day5/oop-bowling/classes/frame.py
@@ -0,0 +1,17 @@
+class Frame:
+    def __init__(self):
+        self.frames = []
+    
+
+    def bowling(self):
+        
+
+
+
+# 1 frame = 
+# {roll : pins_knocked}
+
+# 10th frame = 
+# {roll : pins_knocked} if {1 : 10} then {1 : 10} then {1 : 10}
+# {roll : pins_knocked} if {2 : 10} then {1 : pins_knocked}
+# {roll : pins_knocked} else {2 : num < 10}
