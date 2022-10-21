aa84cfe2cec172d5005fb86b11a8a99803b45c61
Samuel Son <Samuel.Daehan@gmail.com>
2022-10-21 11:21:50 -0500

    update

---
+++ week3/day1/oop_bank_accounts/classes/checking_account.py
@@ -0,0 +1,14 @@
+from classes.account import Account
+
+
+class Checking_Account(Account):
+
+    pass
+
+    def withdraw_using_check(amount):
+        pass
+
+    def reset_checks():
+        pass
+
+    
---
+++ week3/day1/oop_bank_accounts/classes/saving_account.py
@@ -0,0 +1,12 @@
+from classes.account import Account
+
+
+class Saving_Account(Account):
+
+    def __init__(self):
+        pass
+
+
+    def add_interest(rate):
+        pass
+
