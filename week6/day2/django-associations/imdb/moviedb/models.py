ac6a2b24b2eef179afff4a69599d6a005f32af2b
Samuel Son <Samuel.Daehan@gmail.com>
2022-11-07 15:05:18 -0600

    imdb answers

--- week6/day2/django-associations/imdb/moviedb/models.py
+++ week6/day2/django-associations/imdb/moviedb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    pass
+
+
+class Actor(models.Model):
+    movies = models.ManyToManyField(Movie, related_name="actors", through="Role")
+
+
+class Role(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name="roles")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="roles")
+
+    class Meta:
+        unique_together = (("movie", "actor"))
